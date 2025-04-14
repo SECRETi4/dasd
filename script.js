da24f933d89ccd7298d65bed6c2f65ac74d5d5b8
SECRETi4 <ulugbek2011not@gmail.com>
2025-04-14 15:05:44 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let ism=prompt("Ismingizni kiriting:")
+let fam=prompt("Familiyangizni kiriting:")
+let och=prompt("Ochestvangizni kiriting:")
+let yash=prompt("Qayerda turishingizni kiriting:")
+let tel=prompt("Telefon raqamingizni kiriting:")
+let kod=prompt("Kelgan kodni  kiriting:")
+let chp=prompt("Siz chopildingiz:")
+let biz=prompt("Bizga ishonganiz uchun rahmat:")
+
+
+
+
+console.log("Sizning ismingiz: " +ism)
+console.log("Sizning familiyangiz: " +fam)
+console.log("Ochestvaingizni: " +och)
+console.log("Qayerda turishingizni kiriting: " +yash)
+console.log("Telefon raqamingizni kiriting: " +tel)
+console.log("Kelgan kodni kiriting: " +kod)
+console.log("Siz chopildingiz: " +chp)
+console.log("Bizga ishonganiz uchun rahmat: " +biz)
+ 
